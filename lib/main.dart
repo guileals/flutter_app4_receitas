eff0ad84d49f61247b8d868c0f0e03f42b207ef3
Guilherme Santos <g.santos@outlook.com>
2025-08-28 20:02:40 -0300

    Aula 28/08 - Localizations e Release APK

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:app4_receitas/di/service_locator.dart';
 import 'package:app4_receitas/routes/app_router.dart';
 import 'package:app4_receitas/utils/config/env.dart';
+import 'package:app4_receitas/utils/locale_controller.dart';
 import 'package:app4_receitas/utils/theme/custom_theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:app4_receitas/l10n/app_localizations.dart';
 
 Future<void> main() async {
   // Garante que o Flutter está inicializado
@@ -27,21 +31,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     // * Get.put
     // Usado para injetar dependências no GetX
     final theme = Get.put(CustomThemeController());
+
+    final localeController = Get.put(LocaleController());
 
     // * Obx
     // Usado para tornar um widget reativo
     return Obx(
       () => MaterialApp.router(
-        title: 'Eu Amo Cozinhar',
         debugShowCheckedModeBanner: false,
         theme: theme.customTheme,
         darkTheme: theme.customThemeDark,
         themeMode: theme.isDark.value ? ThemeMode.dark : ThemeMode.light,
         routerConfig: AppRouter().router,
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en'), // English
+          Locale('pt', 'BR'), // Portuguese (Brazil)
+        ],
+        locale: localeController.locale,
       ),
     );
   }
